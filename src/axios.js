99d00d2a193c9f5fe556ced05a7c5e8800750bb9
Flavio Santana <f.o.santana@gmail.com>
2020-05-18 17:51:54 -0300

    Criado instância do Axios com valor padrão

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,9 @@ import Axios from 'axios'
 
 import Config from './config/config'
 
-Axios.defaults.baseURL = Config.ROOT_API
+//Axios.defaults.baseURL = Config.ROOT_API
 
-export default Axios
+// Criando instâncias do Axios com valores padrões.
+export default Axios.create({
+    baseURL : Config.ROOT_API
+})
