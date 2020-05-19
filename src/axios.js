bb7e5c295d6827c69e7f0cb265f7642cc7adef11
Flavio Santana <f.o.santana@gmail.com>
2020-05-19 14:11:11 -0300

    Implementado envio de cabeçalhos nas requisições

--- src/axios.js
+++ src/axios.js
@@ -20,25 +20,36 @@ instancia.interceptors.request.use(config => {
     
     console.log ('Interceptando requisição: ', config)
 
-    config.data = {
-        ...config.data,
-        curso: 'Vue JS' 
-    }
-    //return config
-    return new Promise ((resolve, reject)=>{
-        console.log('Fazendo requisição aguardar...')
-        setTimeout(()=>{
-            console.log('Enviando requisição...')
-            resolve(config)
-        },2000)
-    //Pode ser utilizado como ponto global para gerenciamento de erros na aplicação
-    }, error => {
-        
-        console.log('Erro ao fazer requisição: ', error)
+        config.data = {
+            ...config.data,
+            curso: 'Vue JS' 
+        }
 
-        //Passando o erro para frente para ser capturado por algum component
-        return Promisse.reject(error)
-    })
+        //Enviando para todas os tipos de requisições
+        //config.headers.common['Authorization'] = `Bearer ${window.localStorage.getItem('token')}`
+        config.headers.common['Authorization'] = `Bearer token_jwt`
+
+        //Enviando para PUT
+        config.headers.put['Meu-Cabecalho'] = 'Curso Vue JS'
+
+        return config
+        
+        /*return new Promise ((resolve, reject)=>{
+            console.log('Fazendo requisição aguardar...')
+            setTimeout(()=>{
+                console.log('Enviando requisição...')
+                resolve(config)
+            },2000)
+
+        //Pode ser utilizado como ponto global para gerenciamento de erros na aplicação
+        })*/
+    
+},error => {
+        
+    console.log('Erro ao fazer requisição: ', error)
+
+    //Passando o erro para frente para ser capturado por algum component
+    return Promisse.reject(error)
 })
 
 export default instancia
