36fb344cd5bf33932424622eadee7303a734253e
Flavio Santana <f.o.santana@gmail.com>
2020-05-20 18:23:33 -0300

    Implementado rotina para acompanhamento do download

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,30 @@
 
       <TarefasLista />
 
+      <button 
+        class="btn-btn-primary mt-4 m-2"
+        @click="download">
+          Baixar imagem
+      </button>
+
+      <div class="progress">
+        <div 
+          class="progress-bar" 
+          role="progressbar" 
+          :style="{width: progresso + '%'}" 
+          :aria-valuenow="progresso" 
+          aria-valuemin="0" 
+          aria-valuemax="100">
+
+          {{ progresso }} %
+
+        </div>
+      </div>
+
+      <div v-if="imagem">
+        <img :src = "imagem" style="max-width: 100%">
+      </div>
+
     </div>
 
   </div>
@@ -27,6 +51,12 @@ import TarefasLista from './components/TarefasLista.vue'
 export default {
     components: {
         TarefasLista
+    },
+    data(){
+      return {
+        progresso: 0, 
+        imagem: undefined
+      }
     },
     /**
      * Trabalhando com requisições simultâneas no Axios
@@ -79,6 +109,38 @@ export default {
       console.log('Requisições simultâneas:')
       console.log('Tarefa 1:', tarefa1)  
       console.log('Tarefa 3:', tarefa3)
+
+    },
+    methods:  {
+      download(event){
+        Axios.get(
+            'https://images.unsplash.com/photo-1517976487492-5750f3195933?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+            {
+              responseType:'blob',
+              onDownloadProgress:(progressEvent) => {
+                //
+                console.log('Fazendo download...', progressEvent)
+                
+                // 
+                this.progresso = ((progressEvent.loaded / progressEvent.total) * 100).toFixed(0)
+                
+              }
+            }
+          ).then(response  => {
+
+            //
+            console.log('Download concluído', response)
+
+            const reader = new window.FileReader()
+
+            reader.readAsDataURL(response.data)
+          
+            reader.onload = () => {
+              this.imagem = reader.result
+            }
+
+          })
+      }
 
     }
 }
